f60e13efea95ded3541accd06c40e879d20f3783
janak <janak@ADAMOVINA>
2024-01-10 10:48:41 +0100

    Hotovo

--- Bod.cs
+++ Bod.cs
@@ -17,5 +17,6 @@ namespace Maze
             Y = y;
             Hodnota = hodnota;
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,55 +29,109 @@ namespace Maze
         static Bod pocatecniBod = new Bod(4, 7, 0);
         static Bod cilovyBod = new Bod(8, 9, int.MaxValue);
 
+
         static void Main(string[] args)
         {
-            bool cilovyBodNajdeny = false;
-            // Každý bod má ve výchozím stavu maximální možnou hodnotu,
-            // aby se pak dala jednodušeji porovnávat při hledání výsledné cesty
             for (int y = 0; y < body.GetLength(1); y++)
                 for (int x = 0; x < body.GetLength(0); x++)
                     body[x, y] = new Bod(x, y, int.MaxValue);
 
-            int aktualX=pocatecniBod.X;int aktualY = pocatecniBod.Y;
+            int aktualX = pocatecniBod.X; int aktualY = pocatecniBod.Y;
             body[aktualX, aktualY] = pocatecniBod;
 
             Queue<Bod> franta = new Queue<Bod>();
             franta.Enqueue(pocatecniBod);
-            while (cilovyBodNajdeny)
+            while (aktualX != cilovyBod.X || aktualY != cilovyBod.Y)
             {
+                //hore
+                if (aktualY - 1>=0&&prostredi[aktualX, aktualY - 1] == 0 && body[aktualX, aktualY - 1].Hodnota == int.MaxValue)
+                {
 
-            //pravo
-            if (prostredi[aktualX+1, aktualY] == 0)
-            {
-                    aktualX += 1;
-                body[aktualX , aktualY].Hodnota =+ 1;
+                    body[aktualX, aktualY - 1].Hodnota = franta.Peek().Hodnota + 1;
+                    franta.Enqueue(body[aktualX, aktualY - 1]);
+                }
+                //vlavo
+                if (aktualX - 1>=0&&prostredi[aktualX - 1, aktualY] == 0 && body[aktualX - 1, aktualY].Hodnota == int.MaxValue)
+                {
+
+                    body[aktualX - 1, aktualY].Hodnota = franta.Peek().Hodnota + 1;
+                    franta.Enqueue(body[aktualX - 1, aktualY]);
+                }
+                //dole
+                if (aktualY + 1<body.GetLength(1)&&prostredi[aktualX, aktualY + 1] == 0 && body[aktualX, aktualY + 1].Hodnota == int.MaxValue)
+                {
+
+                    body[aktualX, aktualY + 1].Hodnota = franta.Peek().Hodnota + 1;
+                    franta.Enqueue(body[aktualX, aktualY + 1]);
+                }
+                //pravo
+                if (aktualX + 1<body.GetLength(0)&&prostredi[aktualX + 1, aktualY] == 0 && body[aktualX + 1, aktualY].Hodnota == int.MaxValue)
+                {
+
+                    body[aktualX + 1, aktualY].Hodnota = franta.Peek().Hodnota + 1;
+                    franta.Enqueue(body[aktualX + 1, aktualY]);
+                }
+                franta.Dequeue();
+                aktualX = franta.Peek().X; aktualY = franta.Peek().Y;
+
             }
-            //vlavo
-            if (prostredi[aktualX - 1, aktualY] == 0)
+           
+            //ulozit cestu do fronty
+            while (body[aktualX, aktualY].Hodnota != 0)
             {
-                    aktualX -= 1;
-                body[aktualX , aktualY].Hodnota = +1;
-            }
-            //hore
-            if (prostredi[aktualX , aktualY-1] == 0)
-            {
+                //hore
+                 if (body[aktualX, aktualY - 1].Hodnota < body[aktualX, aktualY].Hodnota)
+                {
                     aktualY -= 1;
-                body[aktualX , aktualY].Hodnota = +1;
-            }
-            //dole
-            if (prostredi[aktualX , aktualY+1] == 0)
-            {
-                    aktualX += 1;
-                body[aktualX, aktualY].Hodnota = +1;
-            }
-                franta.Enqueue(body[aktualX, aktualY]);
+                    franta.Enqueue(body[aktualX, aktualY]);
 
-                //finish
-                if (aktualX == cilovyBod.X && aktualY == cilovyBod.Y)
-                    cilovyBodNajdeny = true;
+                }
+                //v pravo
+               else  if (body[aktualX + 1, aktualY].Hodnota < body[aktualX, aktualY].Hodnota)
+                {
+                    aktualX += 1;
+                    franta.Enqueue(body[aktualX, aktualY]);
+
+                }
+                //vlavo
+                else if (body[aktualX - 1, aktualY].Hodnota < body[aktualX, aktualY].Hodnota)
+                {
+                    aktualX -= 1;
+                    franta.Enqueue(body[aktualX, aktualY]);
+
+                }
+                //dole
+                else if (body[aktualX, aktualY + 1].Hodnota < body[aktualX, aktualY].Hodnota)
+                {
+                    aktualY += 1;
+                    franta.Enqueue(body[aktualX, aktualY]);
+
+                }
+
             }
+            Stack<Bod> stack = new Stack<Bod>(franta);
+            foreach (var bod in stack)
+            {
+                Console.Write("[{0};{1}] ", bod.X, bod.Y);
+            }
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //for (var i = 0; i < prostredi.GetLength(0); i++)
+            //{
+            //    for (int ji = 0; ji < prostredi.GetLength(1); ji++)
+            //    {
+            //        if (body[ji, i].Hodnota < 50)
+            //            Console.Write($"{body[ji, i].Hodnota,4}");
+            //        else
+            //        Console.Write($"{"X",4}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            Console.ReadLine();
 
 
         }
+
     }
 }
